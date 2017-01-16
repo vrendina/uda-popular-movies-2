45adeeaa0c06edd03048fcb3f69b140fc28bc358
Victor Rendina <vrendina@gmail.com>
2017-01-15 22:50:14 -0500

    Creation of utils to generate image URL

---
+++ app/src/main/java/software/level/udacity/popularmovies2/api/MovieServiceUtils.java
@@ -0,0 +1,41 @@
+package software.level.udacity.popularmovies2.api;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MovieServiceUtils {
+
+    public static final String TAG = MovieServiceUtils.class.getSimpleName();
+
+    // Base URL for image service from the Movie Database
+    private static final String MOVIE_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
+
+    /**
+     * Generates a URL for downloading an image from The Movie DB image service
+     * @param path API supplied poster_path or backdrop_path
+     * @param size Size of image required (w92, w154, w185, w342, w500, w780, original)
+     * @return URL for downloading image
+     */
+    public static URL buildImageURL(String path, String size) {
+        // Strip any preceeding slash out of the path
+        path = path.replace("/", "");
+
+        Uri uri = Uri.parse(MOVIE_IMAGE_BASE_URL)
+                .buildUpon()
+                .appendPath(size)
+                .appendPath(path)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Problem with constructing URL" + e);
+        }
+
+        return url;
+    }
+}
