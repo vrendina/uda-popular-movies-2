aae7a0fcefd4a06cafa8560cbeac24b08aadd6d3
Victor Rendina <vrendina@gmail.com>
2017-01-17 21:16:07 -0500

    Added initialization for presenter manager

--- app/src/main/java/software/level/udacity/popularmovies2/MovieApplication.java
+++ app/src/main/java/software/level/udacity/popularmovies2/MovieApplication.java
@@ -3,6 +3,7 @@ package software.level.udacity.popularmovies2;
 import android.app.Application;
 
 import software.level.udacity.popularmovies2.api.NetworkManager;
+import software.level.udacity.popularmovies2.ui.PresenterManager;
 
 
 public class MovieApplication extends Application {
@@ -11,8 +12,11 @@ public class MovieApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        // Create the network manager singleton class
+        // Create the network manager singleton object
         NetworkManager.initializeNetworkManager();
+
+        // Create the presenter manager singleton object
+        PresenterManager.initializePresenterManager();
     }
 
 }
