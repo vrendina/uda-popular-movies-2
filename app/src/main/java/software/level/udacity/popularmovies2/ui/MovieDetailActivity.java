c5cf0fac44d7546bd6bb0cb505b45a6a7e5f7c2c
Victor Rendina <vrendina@gmail.com>
2017-01-17 22:19:00 -0500

    Added detail activity

---
+++ app/src/main/java/software/level/udacity/popularmovies2/ui/MovieDetailActivity.java
@@ -0,0 +1,15 @@
+package software.level.udacity.popularmovies2.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import software.level.udacity.popularmovies2.R;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
