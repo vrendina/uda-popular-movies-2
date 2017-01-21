16976b5024f737b276f9bdc6a73b1d4afd8c866d
Victor Rendina <vrendina@gmail.com>
2017-01-21 18:51:05 -0500

    Added dispose method

--- app/src/main/java/software/level/udacity/popularmovies2/ui/Presenter.java
+++ app/src/main/java/software/level/udacity/popularmovies2/ui/Presenter.java
@@ -1,10 +1,15 @@
 package software.level.udacity.popularmovies2.ui;
 
+/**
+ * Base class for all presenters.
+ *
+ * @param <T> View that will be attached to the presenter
+ */
 public class Presenter<T> {
 
     private T view;
 
     public void bindView(T view) { this.view = view; }
-    public void unbindView() { this.view = null; }
-
+    public void unbindView() { view = null; }
+    public void dispose() { PresenterManager.disposePresenter(view.getClass().getSimpleName()); }
 }
