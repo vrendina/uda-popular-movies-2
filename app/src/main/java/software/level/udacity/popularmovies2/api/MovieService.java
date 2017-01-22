de3dce64461bbcb54c9ce9af8adee6d58d432c48
Victor Rendina <vrendina@gmail.com>
2017-01-22 15:09:51 -0500

    Added movie details

--- app/src/main/java/software/level/udacity/popularmovies2/api/MovieService.java
+++ app/src/main/java/software/level/udacity/popularmovies2/api/MovieService.java
@@ -4,6 +4,7 @@ import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import software.level.udacity.popularmovies2.api.model.MovieDetails;
 import software.level.udacity.popularmovies2.api.model.MovieEnvelope;
 import software.level.udacity.popularmovies2.api.model.MovieReviewEnvelope;
 import software.level.udacity.popularmovies2.api.model.MovieTrailerEnvelope;
@@ -14,6 +15,9 @@ public interface MovieService {
 
     @GET("top_rated")
     Observable<MovieEnvelope> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("{id}")
+    Observable<MovieDetails> getMovieDetails(@Query("api_key") String apiKey);
 
     @GET("{id}/reviews")
     Observable<MovieReviewEnvelope> getReviews(@Path("id") int movieId, @Query("api_key") String apiKey);
---
+++ app/src/main/java/software/level/udacity/popularmovies2/api/model/MovieDetails.java
@@ -0,0 +1,8 @@
+package software.level.udacity.popularmovies2.api.model;
+
+/**
+ * Created by vrendina on 1/22/17.
+ */
+
+public class MovieDetails {
+}
